838ddce80c01c80b2b7d07d845d0b9a613605071
KotaDinesh04 <mail.kotadinesh@gmail.com>
2024-04-13 22:03:05 +0530

    Final Commit

--- calculator/src/components/Form.js
+++ calculator/src/components/Form.js
@@ -2,12 +2,10 @@ import React, { useState } from 'react';
 
 export default function Form() {
     const [text, setText] = useState("Enter the values");
-
     const handleOnChange = (event) => {
         setText(event.target.value);
-        console.log(text);
+        // console.log(text);
     }
-
     const handleNumberClick = (number) => () => {
         setText(prevText => prevText + number);
         console.log(text);
@@ -21,14 +19,19 @@ export default function Form() {
     const handleMul = ()=>{
         setText(prevText => prevText + "*");
     }
+
+    const handleDot = ()=>{
+        setText(prevText => prevText + ".");
+
+    }
     const handleDiv = ()=>{
         setText(prevText => prevText + "/");
     }
     const handleAns = ()=>{
         let nums = text.split(/(\+|-|\*|\/)/);
-        let num1 = parseInt(nums[0]);
+        let num1 = parseFloat(nums[0]);
         let op = nums[1];
-        let num2 = parseInt(nums[2]);
+        let num2 = parseFloat(nums[2]);
         //console.log(typeof(num1));
         if(op==="+"){
             setText(num1+num2);
@@ -43,6 +46,10 @@ export default function Form() {
             setText(num1/num2);
         }
     }
+
+    const handleClear = ()=>{
+        setText("");
+    }
     return (
         <div className='container'>
             <input
@@ -55,61 +62,70 @@ export default function Form() {
             />
             <div className="position-relative my-5">
                 <div className="position-absolute top-0 start-0">
-                    <input className="btn btn-primary" type="button" value="0" onClick={handleNumberClick("0")} />
+                    <input className="btn btn-dark" type="button" value="0" onClick={handleNumberClick("0")} />
                 </div>
                 <div className="position-absolute top-0 start-50 translate-middle-x">
-                    <input className="btn btn-primary" type="button" value="1" onClick={handleNumberClick("1")} />
+                    <input className="btn btn-dark" type="button" value="1" onClick={handleNumberClick("1")} />
                 </div>
                 <div className="position-absolute top-0 end-0">
-                    <input className="btn btn-primary" type="button" value="2" onClick={handleNumberClick("2")} />
+                    <input className="btn btn-dark" type="button" value="2" onClick={handleNumberClick("2")} />
                 </div>
             </div>
-            <br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/>
             <div className="position-relative">
                 <div className="position-absolute top-0 start-0">
-                    <input className="btn btn-primary" type="button" value="3" onClick={handleNumberClick("3")} />
+                    <input className="btn btn-dark" type="button" value="3" onClick={handleNumberClick("3")} />
                 </div>
                 <div className="position-absolute top-0 start-50 translate-middle-x">
-                    <input className="btn btn-primary" type="button" value="4" onClick={handleNumberClick("4")} />
+                    <input className="btn btn-dark" type="button" value="4" onClick={handleNumberClick("4")} />
                 </div>
                 <div className="position-absolute top-0 end-0">
-                    <input className="btn btn-primary" type="button" value="5" onClick={handleNumberClick("5")} />
+                    <input className="btn btn-dark" type="button" value="5" onClick={handleNumberClick("5")} />
                 </div>
             </div>
-            <br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/>
             <div className="position-relative">
                 <div className="position-absolute top-0 start-0">
-                    <input className="btn btn-primary" type="button" value="6" onClick={handleNumberClick("6")} />
+                    <input className="btn btn-dark" type="button" value="6" onClick={handleNumberClick("6")} />
                 </div>
                 <div className="position-absolute top-0 start-50 translate-middle-x">
-                    <input className="btn btn-primary" type="button" value="7" onClick={handleNumberClick("7")} />
+                    <input className="btn btn-dark" type="button" value="7" onClick={handleNumberClick("7")} />
                 </div>
                 <div className="position-absolute top-0 end-0">
-                    <input className="btn btn-primary" type="button" value="8" onClick={handleNumberClick("8")} />
+                    <input className="btn btn-dark" type="button" value="8" onClick={handleNumberClick("8")} />
                 </div>
             </div>
-            <br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/>
             <div className="position-relative">
                 <div className="position-absolute top-0 start-0">
-                    <input className="btn btn-primary" type="button" value="+" onClick={handleAdd} />
+                    <input className="btn btn-dark" type="button" value="9" onClick={handleNumberClick("9")} />
                 </div>
                 <div className="position-absolute top-0 start-50 translate-middle-x">
-                    <input className="btn btn-primary" type="button" value="-" onClick={handleMinus}/>
+                    <input className="btn btn-dark" type="button" value="-" onClick={handleMinus}/>
                 </div>
                 <div className="position-absolute top-0 end-0">
-                    <input className="btn btn-primary" type="button" value="*" onClick={handleMul}/>
+                    <input className="btn btn-dark" type="button" value="*" onClick={handleMul}/>
                 </div>
             </div>
-            <br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/>
             <div className="position-relative">
                 <div className="position-absolute top-0 start-0">
-                    <input className="btn btn-primary" type="button" value="/" onClick={handleDiv}/>
+                    <input className="btn btn-dark" type="button" value="/" onClick={handleDiv}/>
                 </div>
                 <div className="position-absolute top-0 start-50 translate-middle-x">
-                    <input className="btn btn-primary" type="button" value="." />
+                    <input className="btn btn-dark" type="button" value="." onClick={handleDot}/>
                 </div>
                 <div className="position-absolute top-0 end-0">
-                    <input className="btn btn-primary" type="button" value="Ans" onClick={handleAns}/>
+                    <input className="btn btn-dark" type="button" value="+" onClick={handleAdd}/>
+                </div>
+            </div>
+            <br/><br/><br/><br/>
+            <div className="position-relative">
+                <div className="position-absolute top-0 start-0">
+                    <input className="btn btn-sm btn-dark" type="button" value="Clear" onClick={handleClear}/>
+                </div>
+                <div className="position-absolute top-0 end-0">
+                    <input className="btn btn-sm btn-dark" type="button" value="Ans" onClick={handleAns}/>
                 </div>
             </div>
         </div>
